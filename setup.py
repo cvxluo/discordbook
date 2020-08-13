267d9709208b6f889eaf843bb1452d0c635c1751
Charlie Luo <cvxluo@gmail.com>
2020-08-13 02:28:40 -0400

    Downgraded required Python version to work with GCP

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="discordbook", 
-    version="0.2.0",
+    version="0.2.1",
     author="Charlie Luo",
     author_email="charliexluo@gmail.com",
     description="A Discord.py module to facilitate easier viewing for large amounts of content.",
@@ -18,5 +18,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires='>=3.5',
 )
